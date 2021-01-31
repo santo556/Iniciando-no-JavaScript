c57685f820f7c87370edafa45e73169c1675b521
santo556 <ve727402gafanhoto@gmail.com>
2021-01-31 10:44:11 -0400

    Aprendendo JS

---
+++ Simples Função/aula4.js
@@ -0,0 +1,10 @@
+/**
+ *JS - Aula4 Função simples
+ @author Aluno André Venâncio dos Santos
+ */
+
+ function alo() {
+     var nome;
+     nome = prompt('Qual é seu nome?');
+     alert(`Aló ${nome}`);
+ }
---
+++ Vincular JS/aula3.js
@@ -0,0 +1,6 @@
+/**
+ * JS - Aula3: Vinculando um documento JavaScript
+ * @author Aluno André Venâncio dos Santos
+ */
+
+ document.write(`<h2>Aluno André Venâncio dos Santos</h2>`);
