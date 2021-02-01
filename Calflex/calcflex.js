4e8241de6835677eca07b42c68a3c5a2ec6e8c2c
santo556 <ve727402gafanhoto@gmail.com>
2021-01-31 21:11:08 -0400

    Calculado Flex

---
+++ Calflex/calcflex.js
@@ -0,0 +1,20 @@
+/**
+ * JS - Aula5: Calculadora FLEX
+ * @author Aluno André Venâncio dos Santos
+ */
+
+/* alert("teste");*/
+var etanol,gasolina;
+function calcular() {
+    etanol = parseFloat(frmFlex.txtEtanol.value.replace(",","."));
+    gasolina = parseFloat(frmFlex.txtGasolina.value.replace(",","."));
+    if (etanol < 0.7 * gasolina) {
+        document.getElementById("status").src="Imagens/etanol.png";
+    }else{
+        document.getElementById("status").src="Imagens/gasolina.png";
+    }
+}
+
+function resetar() {
+    document.getElementById("status").src="Imagens/neutro.png";
+}
