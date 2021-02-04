c15fa6413db8dba03ae7d044761f6fe9e24e7409
santo556 <ve727402gafanhoto@gmail.com>
2021-02-04 18:53:48 -0400

    Slide

--- Aula IX/slide.js
+++ Aula IX/slide.js
@@ -2,4 +2,19 @@
  * JS - Aula9: Simples Slide Show
  */
 
- 
+ var intervalo = 3000;
+
+ function slide1() {
+     document.getElementById("banner").src="Img/banner1.jpg";
+     setTimeout("slide2()",intervalo);
+ }
+
+ function slide2() {
+     document.getElementById("banner").src="Img/banner2.jpg";
+     setTimeout("slide3()",intervalo);
+ }
+
+ function slide3() {
+     document.getElementById("banner").src="Img/banner3.jpg";
+     setTimeout("slide1()",intervalo);
+ }
